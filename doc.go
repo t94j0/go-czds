875e491a723f0194d4acc398054aecc9a132b99d
Max Harley <maxh@maxh.io>
2019-10-14 20:17:14 -0400

    Update doc

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
 // Package czds makes life easy Go developers who work with CZDS. I made this for a long-term research project I'm working on, so it'll likely be supported for a good bit.
 //
-// This is a direct implementation of the ICANN CZDS API which is enumerated in [this PDF](https://github.com/icann/czds-api-client-java/blob/master/docs/ICANN_CZDS_api.pdf)
+// This is a direct implementation of the ICANN CZDS API which is enumerated in this PDF: https://github.com/icann/czds-api-client-java/blob/master/docs/ICANN_CZDS_api.pdf
 package czds
